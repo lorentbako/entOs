c495366a76ed1c3b8ccdebd2b6c8c3a53c2ab42a
lorentbako <lorentbako@gmail.com>
2023-01-27 16:11:39 +0100

    test login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+
   return (
     <div>
       <h1> Login</h1>
 
-      <form>
-        <input placeholder="E-mail" />
-        <input placeholder="Password" />
-        <button type="submit">Submit</button>
+      <form onSubmit={signInWithEmailAndPassword}>
+        <input placeholder="E-mail" value={credentials.email} />
+        <input placeholder="Password" value={credentials.password} />
+        <button type="submit" onSubmit={() => setCredentials()}>
+          Submit
+        </button>
       </form>
     </div>
   );
